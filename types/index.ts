30971a4859a759b6c54156c06fba4d4e4abd9355
Faham Goraya <fahamgoraya@gmail.com>
2026-03-15 18:42:34 -0400

    Removed some unnessary types

--- types/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all types from a single location
-export * from './auth.js';
-export * from './apiKey.js';
-export * from './classroom.js';
-export * from './worksheet.js';
-export * from './translation.js';
-export * from './response.js';
-export * from './request.js';
